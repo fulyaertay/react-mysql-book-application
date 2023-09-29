0723ce3d9bdbbd15962eb36bdbf123844ea4a342
Fulya <fulya.ertay@gmail.com>
2023-09-29 14:10:18 +0300

    connected to mysql

--- client/index.js
+++ client/index.js
@@ -1,6 +1,26 @@
 import express from "express"
-
+import mysql from "mysql"
 const app =express()
+
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"test123",
+    database:"test"
+})
+
+app.get("/",(req,res)=>{
+    res.json("hello this is th backend")
+})
+
+app.get("/books",(req,res)=>{
+    const q="SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) res.json(err)
+        return res.json(data)
+    })
+})
+
 app.listen(8800, ()=>{
     console.log("connected to backend")
 })

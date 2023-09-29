666c17d06d24e2e78b993e950a45252248afed01
Fulya <fulya.ertay@gmail.com>
2023-09-29 15:03:46 +0300

    book details added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Add from './pages/Add';
+import Books from './pages/Books';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
+import Update from './pages/Update';
 function App() {
   return (
     <div className="App">
- 
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Books/>}/>
+      <Route path="/add" element={<Add/>}/>
+      <Route path="/update" element={<Update/>}/>
+    </Routes>
+  </BrowserRouter>
 
     </div>
   );

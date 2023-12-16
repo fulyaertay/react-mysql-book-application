416e645b5949df8d732c74438a57a9687c4637f9
Fulya <fulya.ertay@gmail.com>
2023-12-16 12:11:14 +0300

    finished

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Footer from "./Footer";
+
 
 const Add = () => {
   const [book, setBook] = useState({
